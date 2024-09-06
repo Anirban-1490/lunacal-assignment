e63a4a97c514b3a5a3cebae1e00cfd955fe3e35f
Anirban Pratihar <anirbanpratihar45@gmail.com>
2024-09-07 01:34:28 +0530

    patch: removed next image

--- components/widget-generic/indes.tsx
+++ components/widget-generic/indes.tsx
@@ -11,7 +11,7 @@ export function WidgetGeneric({ children }: IWidgetProps) {
   return (
     <article className="flex  w-[43rem] h-[20rem] bg-[#363C43] shadow-[0.4rem_0.4rem_0.1rem_#000]  rounded-3xl">
       <div className="relative w-[1.8rem] h-[12rem] ml-5 mt-4">
-        <Image src={"/images/icons.png"} alt="" fill />
+        <img src={"/images/icons.png"} alt="icons" />
       </div>
       <div className="w-[37rem]  h-full mx-auto py-5 flex flex-col gap-7">
         {children}
